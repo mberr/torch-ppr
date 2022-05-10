85d488498b32e3a09bfcd5b791b1fee95a8b761d
Charles Tapley Hoyt <cthoyt@gmail.com>
2022-05-10 09:40:24 +0200

    Expose API functions from top-level (#7)
    Also update cookiecutter package in https://github.com/cthoyt/cookiecutter-snekpack/commit/fa032ffc3c718c208d3a03e212aaa299c193de94 to have this be a part by default

--- src/torch_ppr/__init__.py
+++ src/torch_ppr/__init__.py
@@ -1,3 +1,10 @@
 # -*- coding: utf-8 -*-
 
 """(Personalized) Page-Rank computation using PyTorch."""
+
+from .api import page_rank, personalized_page_rank
+
+__all__ = [
+    "page_rank",
+    "personalized_page_rank",
+]
