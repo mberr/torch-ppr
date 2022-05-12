52927ee053fc4e3a25409beab35d2e97fec38c45
Max Berrendorf <berrendorf@dbs.ifi.lmu.de>
2022-05-12 10:19:37 +0200

    Bump version: 0.0.5-dev → 0.0.5

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -27,7 +27,7 @@ copyright = f"{date.today().year}, Max Berrendorf"
 author = "Max Berrendorf"
 
 # The full version, including alpha/beta/rc tags.
-release = "0.0.5-dev"
+release = "0.0.5"
 
 # The short X.Y version.
 parsed_version = re.match(
